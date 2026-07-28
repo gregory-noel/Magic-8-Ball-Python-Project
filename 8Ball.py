6cf9a8b0714b64c65fd7936deb3be844e41ed684
Greg Noel <greg_noel@hotmail.com>
2026-07-28 19:31:05 -0230

    Added a Function for the Ball, increased the responses to the 20 from the original 8-ball and fixed a few errors such as blank questions.  Also added color to the responses depending if they were positive, negative or neutral

--- 8Ball.py
+++ 8Ball.py
@@ -1,5 +1,53 @@
 import random
 
+ 
+#Create a function for the 8ball question, generates a random number and returns an answer based on the random number
+#Positive answers print Green, Nutral print Yellow and Negative print red.
+# Using the built in Python Ansi codes for color.   \033[32m is Green, \033[31m is Red, \033[33m is Yellow \033[0m resets formatting
+def Ball():
+    Result = random.randint(1,20)
+    if Result == 1:
+        Answer = "\033[32m It is certain \033[0m "
+    if Result == 2:
+        Answer = "\033[32m It is decidedly so \033[0m "
+    if Result == 3:
+        Answer = "\033[32m Without a doubt \033[0m "
+    if Result == 4:
+        Answer = " \033[31mDon't count on it \033[0m "
+    if Result == 5:
+        Answer = '\033[31m My reply is no \033[0m '
+    if Result == 6:
+        Answer = '\033[32m Yes definitely \033[0m '
+    if Result == 7:
+        Answer = '\033[31m Very doubtful \033[0m '
+    if Result == 7:
+        Answer = '\033[32m You may rely on it \033[0m '
+    if Result == 9:
+        Answer = '\033[32m As I see it, yes \033[0m '
+    if Result == 10:
+        Answer = '\033[32m Most likely \033[0m '
+    if Result == 11:
+        Answer = '\033[32m Outlook good \033[0m '
+    if Result == 12:
+        Answer = '\033[32m Yes \033[0m '
+    if Result == 13:
+        Answer = '\033[32m Signs point to yes \033[0m '
+    if Result == 14:
+        Answer = '\033[33m  Reply hazy, try again \033[0m '
+    if Result == 15:
+        Answer = '\033[33m  Ask again later \033[0m '
+    if Result == 16:
+        Answer = '\033[33m  Better not tell you now \033[0m '
+    if Result == 17:
+        Answer = '\033[33m  Cannot predict now \033[0m '
+    if Result == 18:
+        Answer = '\033[33m  Concentrate and ask again \033[0m '
+    if Result == 19:
+        Answer = '\033[31m Outlook not so good \033[0m '
+    if Result == 20:
+        Answer = '\033[31m My sources say no \033[0m '
+    return Answer
+
 #create a list of most common english yes no starters
 yes_no_starters = ["Am", "Is", "Are", "Was", "Were", "Do", "Does", "Did", "Have", "Has", "Had", 
     "Can", "Could", "Will", "Would", "Shall", "Should", "May", "Might", "Must", 
@@ -10,45 +58,39 @@ yes_no_starters = ["Am", "Is", "Are", "Was", "Were", "Do", "Does", "Did", "Have"
     "Hear", "Mind", "Care", "Innit", "Eh"]
 
 PlayAgain = str('Y')
-#print(PlayAgain)
+
 
 while PlayAgain == str('Y'):
-    #print(PlayAgain)
-    Question = input('I am the Magic 8 Ball.  I know all.  Ask me any Yes/No question.')
-  
+    
+  #I was getting a index error if I left the question blank so this makes sure it's not blank.  
+    while True:
+        Question = input('I am the Magic 8 Ball.  Ask me any Yes/No question.')
+        if Question == "":
+            print("Error: Input cannot be left blank! Please try again.")
+        else:
+            break
+
+    print("You asked: ", Question)
 
 
-# Check in the question starts with one of the first word starters. 
+# Check if the question starts with one of the first word starters. 
 # Create a variable of just the first word
     First_Word = Question.split()[0]
-    #print(First_Word)
-#Make the first word vaiable a title to match the starters list.
+#Make the first word vaiable title case to match the starters list.
     First_Word_Title = First_Word.title()
-    #print(First_Word_Title)
-#check if the first word is in the starters list
+
+#if the first word is in the starters list run the 8_Ball function, if not Say its not a yes no question
+    
     Is_Starter = First_Word_Title in yes_no_starters
 
 # if it's in the list create a random number between 1-6 and print the cooresponding response
     if Is_Starter:
-        Result = random.randint(1,6)
-        print("You asked: ", Question)
-        print('May answer is: ')
-        if Result == 1:
-            print('It is certain')
-        if Result == 2:
-            print('It is decidedly so')
-        if Result == 3:
-            print('Without a doubt')
-        if Result == 4:
-            print("Don't count on it")
-        if Result == 5:
-            print('My reply is no')
-        if Result == 6:
-            print('Very doubtful')
+        print("The Magic 8-Ball says:" , Ball())
     else: 
         print('That was not a Yes/No question!')
-#Ask to play again, if Y or Yes continue while loop
-    PlayAgain = str(input('Would you like to Play Y/N'))
+#Ask to play again, if Y continue while loop if anything else exit and end
+    PlayAgain = str(input("Press Y to ask another question.  Or press any other key to stop."))
     PlayAgain = PlayAgain.capitalize()
+    
    
 print('Thanks for Playing!')
